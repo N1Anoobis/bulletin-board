1ef6ebb4c4db60722c08fb7c02f953241c2bc5fe
N1Anoobis <mrbond84@hotmail.com>
2020-10-25 18:48:17 +0100

    Add heroku setup

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,12 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error: ' + err));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*',(req, res)=> {
+    res.sendFile(path.resolve(__dirname,'build','index.html'));
+  })
+}
 /* START SERVER */
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
