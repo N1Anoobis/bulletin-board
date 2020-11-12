57ee44ecf83692c87fcb85d7d327951f043c3be8
N1Anoobis <mrbond84@hotmail.com>
2020-11-13 00:23:06 +0100

    Add heroku script

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*',(req, res)=> {
     res.sendFile(path.resolve(__dirname,'build','index.html'));
-  })
+  });
 }
 /* START SERVER */
 const port = process.env.PORT || 8000;
