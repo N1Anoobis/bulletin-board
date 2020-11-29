9e145bd8387a96e746dab34ac21b9f20a03b936c
N1Anoobis <mrbond84@hotmail.com>
2020-11-29 17:31:31 +0100

    Add correct env variables to netlify

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <Auth0Provider
-    domain= {process.env.DOMAIN}
-    clientId= {process.env.CLIENT_ID}
+    domain= {DOMAIN}
+    clientId= {ID}
     redirectUri={window.location.origin}>
     <App />
   </Auth0Provider>,
